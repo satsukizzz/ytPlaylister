9b05a12770d0bc056f56a4aa3e00225b9f9edce9
satsukizzz <dn.asp01@gmail.com>
2020-06-09 23:54:34 +0900

    add some features, like トマトプリン

--- index.js
+++ index.js
@@ -3,22 +3,42 @@ const client = new Discord.Client();
 const prefix = "!";
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('bot is ready!');
 });
 
 client.on('message', message => {
+  if (message.isMemberMentioned(client.user)) {
+    message.reply( 'My prefix is "!".');
+    return;
+  }
+  if (message.content.startsWith("!p" || "-p")) {
+    // TODO calling music bot with youtube musics...... hook them if could
+    return;
+  }
   if(!message.content.startsWith(prefix) || message.author.bot) return;
-  const args = message.content.slice(prefix.length).split(' ');
+  const args = message.content.slice(prefix.length).split(/ +/);
   console.log(args);
   const command = args.shift().toLowerCase();
-  console.log(command);
-console.log(args);
-  if(message.content === "!ping"){
-    message.channel.send('Pong.');
-  } else {
-  console.log(message.content);
-  }
 
+  if (command === "ping") {
+    message.channel.send('Pong.');
+  } else if (command === "beep") {
+    message.channel.send('Boop.');
+  } else if (command === "トマトプリン") {
+    message.channel.send('munch munch.... yummy!');
+  } else if (command === 'server') {
+    message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+  } else if (command === 'user-info') {
+    message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+  } else if (command === 'info') {
+    if (!args.length) {
+      return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+    } else if (args[0] === 'foo') {
+      return message.channel.send('bar');
+    }
+
+    message.channel.send(`First argument: ${args[0]}`);
+  }
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
